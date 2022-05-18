99518b3f09055e422846b5996704479ea114903d
SpiderBee1 <goodm0139@gmail.com>
2022-05-18 16:00:10 -0400

    added async action dispatch with redux thunk and spinner component for loading

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,40 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { loggerMiddleware } from "./middleware/logger";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
+const persistConfig = {
+  key: "root",
+  storage,
+  //only persist cart
+  whitelist: ['cart'],
 };
 
-const middleWares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//logger or loggerMiddleware
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+//'redux devtools' browser addon compatibility modification
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyByzP5XxaDc5BPb8i70tr7N15e-K1rJE5w",
+  authDomain: "crwn-clothing-db-74139.firebaseapp.com",
+  projectId: "crwn-clothing-db-74139",
+  storageBucket: "crwn-clothing-db-74139.appspot.com",
+  messagingSenderId: "420412325229",
+  appId: "1:420412325229:web:e771e34348cd7527de2e64",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
